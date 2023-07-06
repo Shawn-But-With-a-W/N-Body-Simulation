b356696a042381056be435119509c969368bc498
Shawn <shaochen.li@hotmail.com>
2023-07-06 18:40:56 +1000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+class Body {
+    constructor (radius, mass, x, y, vX, vY, aX, aY) {
+        this.radius = radius;
+        this.mass = mass;
+        this.x = x;
+        this.y = y;
+        this.vX = vX;
+        this.vY = vY;
+        this.aX = aX;
+        this.aY = aY;
+    }
+}
