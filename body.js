9919c20b11d55b343f3640a4cba964b72eda61c1
Shawn <shaochen.li@hotmail.com>
2023-07-22 12:55:30 +1000

    Change x y values to record form

--- body.js
+++ body.js
@@ -1,75 +1,76 @@
 const G =  0.0000000000667428; // Gravitational constant, used in calculation of force
 let t = 0.5;
+let bodies = []; // Array of all bodies present
 
 class Body {
-    constructor (radius, mass, colour, x, y, vX, vY) {
+    constructor (radius, mass, colour, pos, vel) {
         this.radius = radius; // (m)
         this.mass = mass; // (kg)
         this.colour = colour; // Yes I am spelling it as 'colour'
-        // newX and newY are used for drawing the body, x and y are used for calculations
-        this.x  = this.newX = x; // (m)
-        this.y = this.newY = y;
-        this.vX = vX; // (m/s)
-        this.vY = vY;
-        this.aX = this.aY = 0; // (m/s^2)
-        this.fX = this.fY = 0; // (N)
-        
+        // newPos is used for drawing the body, pos is used for calculations
+        this.pos = pos; // (m)
+        this.newPos = pos;
+        this.vel = vel; // (m/s)
+        this.acc = {x:0, y:0}; // (m/s^2)
+        this.force = {x:0, y:0}
+
         this.draw();
+        bodies.push(this);
     }
 
     draw() {
         ctx.beginPath();
             ctx.fillStyle = this.colour;
-            ctx.arc(this.newX, this.newY, this.radius, 0, 2*Math.PI);
+            ctx.arc(this.newPos.x, this.newPos.y, this.radius, 0, 2*Math.PI);
             ctx.fill();
         ctx.closePath();
     }
 
     updateForce() {
-        this.fX = 0;
-        this.fY = 0;
+        this.force.x = 0;
+        this.force.y = 0;
 
         for (let body of bodies) {
             // Gravitational force only applied by bodies other than the current body itself
             if (body != this) {
                 // Finding the magnitude and angle from horizontal
-                let f = (G * this.mass * body.mass) / (Math.pow(this.x - body.x, 2) + Math.pow(this.y - body.y, 2));
-                let theta = Math.atan(Math.abs((this.y - body.y) / (this.x - body.x)));
+                let force = (G * this.mass * body.mass) / ((this.pos.x - body.pos.x) ** 2 + (this.pos.y - body.pos.y) ** 2);
+                let theta = Math.atan(Math.abs((this.pos.y - body.pos.y) / (this.pos.x - body.pos.x)));
 
-                // Decomposing into x and y components
-                if (body.x > this.x) {
-                    this.fX += f * Math.cos(theta);
+                // Decomposing into x and y components and adding to the final force vector
+                if (body.pos.x > this.pos.x) {
+                    this.force.x += force * Math.cos(theta);
                 }
                 else {
-                    this.fX -= f * Math.cos(theta);
+                    this.force.x -= force * Math.cos(theta);
                 }
-                if (body.y > this.y) {
-                    this.fY += f * Math.sin(theta);
+                if (body.pos.y > this.pos.y) {
+                    this.force.y += force * Math.sin(theta);
                 }
                 else {
-                    this.fY -= f * Math.sin(theta);
+                    this.force.y -= force * Math.sin(theta);
                 }
             }
         }
     }
 
-    updateAcceleration() {
-        this.aX = this.fX / this.mass;
-        this.aY = this.fY / this.mass;
+    updateAcc() {
+        this.acc.x = this.force.x / this.mass;
+        this.acc.y = this.force.y / this.mass;
     }
 
-    updateVelocity() {
-        this.vX += this.aX * t;
-        this.vY += this.aY * t;
+    updateVel() {
+        this.vel.x += this.acc.x * t;
+        this.vel.y += this.acc.y * t;
     }
 
-    updatePosition() {
-        this.newX += this.vX * t;
-        this.newY += this.vY * t;
+    updatePos() {
+        this.newPos.x += this.vel.x * t;
+        this.newPos.y += this.vel.y * t;
     }
 
-    updateCalcPosition() {
-        this.x = this.newX;
-        this.y = this.newY;
+    updateCalcPos() {
+        this.pos.x = this.newPos.x;
+        this.pos.y = this.newPos.y;
     }
 }
--- render.js
+++ render.js
@@ -4,8 +4,6 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
-let bodies = []; // Array of all bodies currently present in the canvas
-
 let zoomLevel = 1; // Zoom level to be used for canvas
 let translate = {x:0, y:0};
 
@@ -18,6 +16,7 @@ function resizeCanvas() {
     // Change canvas to cartesian standards
     ctx.translate(canvas.width/2 + translate.x, canvas.height/2 + translate.y);
     ctx.scale(zoomLevel, -zoomLevel);
+
     // Draw a rectangle over the entire canvas
     ctx.clearRect((-canvas.width/2 - translate.x) / zoomLevel, (-canvas.height/2 + translate.y) / zoomLevel, canvas.width/zoomLevel, canvas.height/zoomLevel);
     // Redraw everything
@@ -49,11 +48,8 @@ function pan(event) {
 
 let interval = setInterval(update, 1);
 
-let body1 = new Body(50, Math.pow(10, 15), "red", -150, -150, 5, -5);
-let body2 = new Body(50, Math.pow(10, 15), "blue", 150, 150, -5, 5);
-let body3 = new Body(50, Math.pow(10, 15), "green", 500, 500, -5, 5);
-
-bodies = [body1, body2, body3];
+let body2 = new Body(50, Math.pow(10, 15), "blue", {x:150, y:150}, {x:7, y:-7});
+let body1 = new Body(50, Math.pow(10, 15), "red", {x:-150, y:-150}, {x:-7, y:7});
 
 function update() {
     // Draw a rectangle over the entire canvas
@@ -62,33 +58,33 @@ function update() {
     // Update values and redraw the bodies
     for (let body of bodies) {
         body.updateForce();
-        body.updateAcceleration();
-        body.updateVelocity();
-        body.updatePosition();
+        body.updateAcc();
+        body.updateVel();
+        body.updatePos();
         body.draw();
     }
 
     // Change the previous and current position values to be identical, separately to not intefere with calculating force (which is based on distance)
     for (let body of bodies) {
-        body.updateCalcPosition();
+        body.updateCalcPos();
 
         if (body == body1) {
             document.getElementById("body1").innerText = `
-                force: ${body1.fX}, ${body1.fY}
-                acceleration: ${body1.aX}, ${body1.aY}
-                velocity: ${body1.vX}, ${body1.vY}
-                new position: ${body1.newX}, ${body1.newY}
-                position: ${body1.x}, ${body1.y}
+                force: ${body1.force.x}, ${body1.force.y}
+                acceleration: ${body1.acc.x}, ${body1.acc.y}
+                velocity: ${body1.vel.x}, ${body1.vel.y}
+                new position: ${body1.newPos.x}, ${body1.newPos.y}
+                position: ${body1.pos.x}, ${body1.pos.y}
             `;
         }
 
         else if (body == body2) {
             document.getElementById("body2").innerText = `
-                force: ${body2.fX}, ${body2.fY}
-                acceleration: ${body2.aX}, ${body2.aY}
-                velocity: ${body2.vX}, ${body2.vY}
-                new position: ${body2.newX}, ${body2.newY}
-                position: ${body2.x}, ${body2.y}
+                force: ${body2.force.x}, ${body2.force.y}
+                acceleration: ${body2.acc.x}, ${body2.acc.y}
+                velocity: ${body2.vel.x}, ${body2.vel.y}
+                new position: ${body2.newPos.x}, ${body2.newPos.y}
+                position: ${body2.pos.x}, ${body2.pos.y}
             `;
         }
     }
