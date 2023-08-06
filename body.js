0637d2e8db8d0fde24a304abd1bc8f91798b608e
Shawn <shaochen.li@hotmail.com>
2023-08-07 09:49:24 +1000

    Remove console.log

--- body.js
+++ body.js
@@ -7,6 +7,5 @@ let vel = {x:0, y:0};
 canvas.addEventListener("mouseup", createBody);
 
 function createBody(event) {
-    new Body(radius, mass, colour, {x:(event.clientX - canvas.width/2 - translate.x) / zoomLevel, y:-(event.clientY - canvas.height/2 - translate.y) / zoomLevel}, {x:vel.x, y:vel.y});
-    console.log(event.clientX, event.clientY);
+    new Body(radius, mass, colour, {x:(event.clientX - canvas.width/2 - translateLevel.x) / zoomLevel, y:-(event.clientY - canvas.height/2 - translateLevel.y) / zoomLevel}, {x:vel.x, y:vel.y});
 }
