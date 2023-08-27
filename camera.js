f4d790a35aed951e4ad1746753203fc75fb16586
Shawn <shaochen.li@hotmail.com>
2023-08-28 09:48:40 +1000

    Small backup version

--- camera.js
+++ camera.js
@@ -25,7 +25,7 @@ function resizeCanvas() {
 window.addEventListener("wheel", zoom);
 
 function zoom(event) {
-    zoomLevel += event.deltaY * -0.000001;
+    zoomLevel += event.deltaY * -0.0000001;
     zoomLevel = Math.min(Math.max(0.000001, zoomLevel), 0.5); // Add a maximum and minimum value to the zoom
     resizeCanvas();
 }
